658153904644cf9c18763bb6379cac662c1791e6
Nic Wilson <nicwilson@gmail.com>
2017-04-20 07:47:58 -0500

    Fix User Buy Method

--- client/scripts/client.js
+++ client/scripts/client.js
@@ -1,62 +1,50 @@
 let myApp = angular.module('myApp', []);
 
 
-myApp.factory('GameService', [function(){
-  let startingPrice = 5;
-  class MarketItem {
-    constructor(name){
-    this.name = name;
-    this.price = startingPrice;
-    setInterval(() =>{
-      this.fluctuate();
-      console.log(this.price);
-    }, 1000);
-  }
-  fluctuate(){
-    this.price += (Math.random()-0.5);
-    this.price = Math.max(0.5, this.price);
-    this.price = Math.min(9.99, this.price);
-  }
-}
+myApp.factory('GameService', [function() {
 
-class User{
-  let startingPrice = 5;
-  class MarketItem {
-    constructor(name){
-    this.name = name;
-    this.price = startingPrice;
-    setInterval(() =>{
-      this.fluctuate();
-    }, 1000);
-  }
-  fluctuate(){
-    this.price += (Math.random()-0.5);
-    this.price = Math.max(0.5, this.price);
-    this.price = Math.min(9.99, this.price);
-  }
-}
+    class MarketItem {
+        constructor(name, price) {
+            this.name = name;
+            this.price = price;
+            setInterval(() => {
+                this.fluctuate();
+            }, 1000);
+        }
+        fluctuate() {
+            this.price += (Math.random() - 0.5);
+            this.price = Math.max(0.5, this.price);
+            this.price = Math.min(9.99, this.price);
+        }
+    }
 
-class User{
-  constructor(){
-    this.cash = 0;
-    this.inventory = {};
-  }
-  buy(item){
-    // let prevQty = (this.inventory[item.name].qty || 0);
-    // let prevAvgPrice = (this.inventory[item.name].avgPrice || 0);
-    // this.inventory[item.name].qty =  this.inventory[item.name].qty ? this.inventory[item.name].qty++ : 1;
-    // let totalCost = prevQty*prevAvgPrice + item.price;
-    // this.inventory[item.name].avgPrice = totalCost / this.inventory[item.name].qty;
-    // console.log(this.inventory);
-  }
-  sell(item){
+    class User {
+        constructor() {
+            this.cash = 0;
+            this.inventory = {};
+        }
+        buy(item) {
 
-  }
-}
+            if (!this.inventory[item.name]) {
+                this.inventory[item.name] = {
+                    qty: 0,
+                    avgPrice: 0
+                };
+            }
+            let invSlot = this.inventory[item.name];
+            let prevQty = invSlot.qty;
+            let prevAvgPrice = invSlot.avgPrice;
+            invSlot.qty++;
+            let totalCost = prevQty * prevAvgPrice + item.price;
+            invSlot.avgPrice = totalCost / invSlot.qty;
+            console.log(this.inventory);
+        }
+        sell(item) {
 
-let banana = new MarketItem('banana');
-let bob = new User()
+        }
+    }
 
-}
+    let banana = new MarketItem('banana', 5);
+    let bob = new User()
 
 }]);
